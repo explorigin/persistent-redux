c357d605c284fdc9ce24ea20bb9d313a1a859b03
Timothy Farrell <explorigin@gmail.com>
2015-12-05 13:26:31 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,118 @@
+# Persistent-Redux
+
+Persistent-Redux will save your Redux state to a [PouchDB](http://pouchdb.com/) instance and restore it on the next page load.
+
+## Installation
+
+To install the stable version:
+
+```
+npm install --save persistent-redux
+```
+
+This assumes that you’re using [npm](https://www.npmjs.com/) package manager with a module bundler like [Webpack](http://webpack.github.io) or [Browserify](http://browserify.org/) to consume [CommonJS modules](http://webpack.github.io/docs/commonjs.html).
+
+## Usage
+
+```
+// ... other imports ...
+import persistentStore from './middlewares/persistentStore';
+
+const options = {
+	db: new PouchDB('AppState', {storage: 'persistent'}),
+	ignoreActions: (() => true),
+	blobSupport: true
+};
+
+# Pulling initial state from the database is an asynchronous action.
+persistentStore(options).then((persistentMiddleware) => {
+	const createStoreWithMiddleware = compose(
+		persistentMiddleware,
+    // ... other middlewares ...
+	)(createStore);
+
+	let store = createStoreWithMiddleware(RootReducer);
+
+	render((
+		<Provider store={store}>
+			// ... normal JSX Components ...
+		</Provider>
+	), document.body);
+}).catch((err) => {
+	alert('Could not initialize persistent middleware.');
+	console.error(err);
+});
+```
+
+## Options
+
+### db
+Persistent-Redux uses [PouchDB](http://pouchdb.com/) as its back-end.  Pass in the PouchDB database that you wish to use for persistence.
+It's recommended to specify [persistent storage for Firefox](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Browser_storage_limits_and_eviction_criteria).  PouchDB allows this through the ([currently undocumented](https://github.com/pouchdb/pouchdb/issues/4315)) `storage` option like `new PouchDB('AppState', {storage: 'persistent'})`.
+
+### ignoreActions
+
+`ignoreActions` is a function that returns a boolean if an action should be persisted.
+
+### blobSupport
+
+**Currently forced to `true`.  Version 0.7.0 will allow for false.***
+`blobSupport` is a boolean (default to `false`) that enables support for saving Blobs as members of actions.  If you wish to support Blobs:
+
+1. Set `blobSupport: true`
+2. Add an `_attachments` property in your action as a list of strings of object paths to the Blobs.  For example (type annotations added for clarity):
+
+	```
+	const REGISTER_PARTY = "party_on";
+
+	function throwAParty(occasion: String, location: String, clipartImage: Blob) {
+		return {
+			type: REGISTER_PARTY,
+			payload: {
+				occasion,
+				location,
+				clipart: clipartImage
+			},
+			_attachments: ['payload.clipart']
+		};
+	}
+	```
+
+## Notes
+
+### Supported Data-types
+
+Persistent-Redux supports all of the same [data-types that PouchDB supports](http://pouchdb.com/faq.html#data_types).
+
+### Asynchronous Process
+
+Because most I/O is asynchronous, the persistent store is also asynchronous. The flow of the persistent store looks like this:
+
+1. Fetch existing actions from the database.
+2. Run these actions through the root reducer to build the initial state.
+3. Return a middleware function that will apply the initial state to the created store and capture actions.
+
+When an action is dispatched to the wrapped store:
+1. The persistent middleware will serialize and save the action.
+2. When the action is saved, the changes feed will report back to the middleware which will re-dispatch the action to propagate to other middlewares and eventually the reducers to change the current state.  If PouchDB is a remote database, this could slow down the interaction of your app.
+
+### [Redux-Router](https://github.com/rackt/redux-router)
+
+It is not recommended to use Persistent-Redux with [Redux-Router](https://github.com/rackt/redux-router) at this time because it [includes unserializable functions in its actions](https://github.com/rackt/redux-router/issues/105).  Until this is rectified, these actions cannot be persistent.
+
+If you still wish to use Redux-Router, filter out its actions by setting `ignoreActions` to bypass them:
+
+```
+{
+	ignoreActions: (action) => action.type.indexOf('@@reduxReactRouter') === 0
+}
+```
+
+## TODO
+
+There are some things remaining:
+
+- 0.7.0 - factor out blob support
+- 0.8.0 - squash actions into an initial state
+- 0.9.0 - add optimistic dispatching
+- 1.0.0 - test coverage
---
+++ src/index.js
@@ -0,0 +1,176 @@
+import { applyMiddleware } from 'redux';
+
+// Constants
+const FEED_CHANGED = '@@persistentStore/feedChanged';
+const REDUX_ACTION_TYPE = 'reduxAction';
+const DESIGN_DOC = {
+	_id: '_design/fetchReduxActionHistory',
+	views: {
+		fetchReduxActionHistory: {
+			map: `function mapFun(doc) {
+				if (doc.type === "${REDUX_ACTION_TYPE}") {
+					emit(doc.id);
+				}
+			}`
+		}
+	}
+};
+
+// Functions
+function dispatchSavedActionToStore(store, init, record) {
+	let payload = record.doc.payload;
+	let attachments = record.doc._attachments;
+
+	if (attachments !== undefined) {
+		Object.keys(attachments).forEach((pathString) => {
+			let path = pathString.split('.');
+			let currentLocation = payload;
+			while (path.length > 1) {
+				currentLocation = currentLocation[path.shift()];
+			}
+			currentLocation[path[0]] = attachments[pathString];
+		});
+	}
+
+	store.dispatch({
+		type: FEED_CHANGED,
+		init: init,
+		payload: payload
+	});
+}
+
+function extractAttachments(action) {
+	// FIXME - Copying the action may be unnecessary and slow here.
+	let payload = { ...action }, attachments = null;
+
+	if (Array.isArray(action._attachments)) {
+		attachments = {};
+		action._attachments.forEach((pathString) => {
+			let path = pathString.split('.');
+			let currentLocation = payload;
+			while (path.length > 1) {
+				currentLocation = currentLocation[path.shift()];
+			}
+			const lastProp = path[0];
+			const data = currentLocation[lastProp];
+			currentLocation[lastProp] = '_attachment';
+
+			if (data instanceof Blob) {
+				attachments[pathString] = {
+					'content_type': data.type,
+					'data': data
+				};
+			} else {
+				// FIXME - Here be dragons
+				attachments[pathString] = {
+					'content_type': 'text/plain',
+					'data': data
+				};
+			}
+		});
+		action._attachments = undefined;
+	}
+
+	return {
+		payload,
+		attachments
+	};
+}
+
+function persistenceMiddleware(db, options) {
+	const ignoredActionPrefixes = new Set();
+
+	let { startingSequence, ignoreActions } = options;
+	var sequence = startingSequence;
+
+	ignoreActions.forEach(ignoredActionPrefixes.add.bind(ignoredActionPrefixes));
+
+	return (/* store */) => next => {
+		var ignoredActionQueue = [], waitingOnAsyncActions = 0;
+
+		return action => {
+			if (action.type === FEED_CHANGED) {
+				next(action.payload);
+				if (waitingOnAsyncActions === 1 && ignoredActionQueue) {
+					let queue = ignoredActionQueue.slice();
+					queue.forEach(next);
+					ignoredActionQueue = ignoredActionQueue.splice(0, queue.length);
+				}
+				if (!action.init) {
+					waitingOnAsyncActions--;
+				}
+			} else if (ignoredActionPrefixes.has(action.type.split('/')[0])) {
+				if (waitingOnAsyncActions) {
+					ignoredActionQueue.push(action);
+				} else {
+					next(action);
+				}
+			} else {
+				// TODO - detect unserializable actions in DEBUG mode
+				sequence++;
+				waitingOnAsyncActions++;
+				const { payload, attachments } = extractAttachments(action);
+				let doc = {
+					_id: `RA-${sequence}`,
+					type: REDUX_ACTION_TYPE,
+					payload: payload
+				};
+				if (attachments) {
+					doc._attachments = attachments;
+				}
+				db.put(doc).catch((err) => {
+					console.log('Could not serialize action: ', action);
+					console.log(err);
+				});
+			}
+		};
+	};
+}
+
+export default function persistentStore({ db, ignoreActions }) {
+	var middleware, savedActions = [];
+
+	const createStoreWrapper = (createStore) => (reducer, initialState={}) => {
+		let store = applyMiddleware(middleware)(createStore)(reducer, initialState);
+		savedActions.forEach(dispatchSavedActionToStore.bind(null, store, true));
+
+		db.changes({
+			live: true,
+			include_docs: true,
+			since: 'now',
+			attachments: true,
+			binary: true
+		}).on('change', dispatchSavedActionToStore.bind(null, store, false));
+
+		return store;
+	};
+
+	return db.info().then((info) => {
+		let { update_seq } = info;
+		middleware = persistenceMiddleware(
+			db,
+			{
+				startingSequence: update_seq,
+				ignoreActions
+			});
+		return db.put(DESIGN_DOC);
+	}).catch((err) => {
+		if (err.status !== 409) { throw err; }
+		// ignore if doc already exists
+	}).then(() => {
+		return db.query(
+			'fetchReduxActionHistory',
+			{include_docs: true, attachments: true, binary: true}
+		);
+	}).then((result) => {
+		console.info(`Found ${result.total_rows} saved action(s).`);
+		savedActions = result.rows;
+		return createStoreWrapper;
+	}).catch((err) => {
+		if (err.message === 'missing') {
+			console.info('Could not find saved state.');
+			return createStoreWrapper;
+		}
+		console.error(err);
+	});
+}
